50774bfbfeab93821007e1a4d18ed99067ae31e8
carlosJimenez2 <42255224+carlosJimenez2@users.noreply.github.com>
2018-10-03 00:11:33 -0500

    Add files via upload

---
+++ Faltas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_9
+{
+    class Faltas : IDatos
+    {
+        private string nombre;
+        private double clavetrabajador;
+        private string fecha;
+        private double numerodedias;
+
+
+        public Faltas()
+        {
+            nombre = "";
+            clavetrabajador = 0.00;
+            fecha = "";
+            numerodedias = 0.0;
+        }
+
+        public Faltas(string nom, double ct, string date, double numd)
+        {
+            nombre = nom ;
+            clavetrabajador = ct;
+            fecha = date;
+            numerodedias = numd;
+ 
+
+
+        }
+
+        public void muestraDiasconfaltas()
+        {
+            Console.WriteLine("El trabajador {0}", nombre);
+            Console.WriteLine("Con la clave: {0}", getclavetrabajador());
+            Console.WriteLine("y fecha: {0}", fecha);
+            Console.WriteLine("Tiene: {0} faltas acumuladas", numerodedias);
+            Console.WriteLine("    ");
+
+        }
+        public double getclavetrabajador()
+        {
+            return clavetrabajador;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Faltas Trabajador1 = new Faltas("Jimenez Arellano Carlos", 314104326 ,"02/10/2018", 2);
+            Faltas Trabajador2 = new Faltas("INGRESE NOMBRE QUE MAS LE GUSTE", 123456789, "02/10/2018", 3);
+
+            Trabajador1.muestraDiasconfaltas();
+            Trabajador2.muestraDiasconfaltas();
+
+            Console.ReadLine();
+        }
+    }
+}
